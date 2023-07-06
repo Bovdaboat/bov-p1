f17ca171e9197c6939ebc45404c5de52cd6c0c32
bovdaboat <ignatgabov@gmail.com>
2023-07-06 16:20:34 -0700

    made all of front end page for the pugs

---
+++ src/app/comp/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-pink-500">
+            <a className="btn btn-ghost normal-case text-xl text-base-300/90">Bov_Pugzzz</a>
+        </div>
+    )
+}
+
+export default Navbar
